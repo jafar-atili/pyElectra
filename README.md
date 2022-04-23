bb7165485ce97a93dd007db33fa0ac255d8f087b
Jafar Atili <at.jafar@outlook.com>
2022-04-23 20:39:49 +0300

    fixes

--- README.md
+++ README.md
@@ -6,26 +6,49 @@ Based on: https://github.com/nitaybz/homebridge-electra-smart
 
 Usage:
 
-import electra
+import asyncio
+import aiohttp
+from electra import *
 
 async def main():
     session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False), timeout=aiohttp.ClientTimeout(total=10))
-    api = ElectraAPI('IMEI_KEY', 'TOKEN_KEY', session)
- 
-    devices = await api.get_devices()
-    if (devices):
-        for device in devices:
-            await api.get_last_telemtry(device)
-            assert(isinstance (device, ElectraAC))
-            print(device.id)
-            if device.id == 93813:
-                device.turn_on()
-                device.set_fan_speed(OPER_FAN_SPEED_LOW)
-                device.set_temp(26)
-                await api.set_state(device)
-                
+    api = ElectraAPI(session)
 
-    await session.close()
+    # User phone number
+    phone_number = "0521234567"
+    # Generate token
+    imei = generate_imei()
+    try:
+        resp = await api.generate_new_token(phone_number=phone_number, imei=imei)
+    except ElectraApiError as e:
+        # handle error
+        pass
+
+    otp = input("Enter the OTP you recieved via SMS")
+    # more error handling
+    if resp[electra.ATTR_STATUS] == electra.STATUS_SUCCESS:
+        if resp[electra.ATTR_DATA][electra.ATTR_RES] != electra.STATUS_SUCCESS:
+            # Wrong phone number or unregistered user
+            sys.exit(1)
+     
+        resp = await api.validate_one_time_password(otp=otp, imei=imei, phone_number=phone_number)
+        if resp[electra.ATTR_DATA][electra.ATTR_RES] == electra.STATUS_SUCCESS:
+            token = resp[electra.ATTR_DATA][electra.ATTR_TOKEN]
+        else:
+            # wrong OTP
+            sys.exit(1)
+    
+    ac_devices = api.get_devices()
+    for ac in ac_devices:
+        assert(ac, ElectraAirConditioner)
+        if ac.name == "Saloon AC":
+            ac.turn_on()
+            ac.set_mode(OPER_MODE_COOL)
+            ac.set_temperature(17)
+            ac.set_fan_speed(OPER_FAN_SPEED_HIGH)
+            ac.set_vertical_swing(OPER_ON)
+            api.set_state(ac)  # This will send the conf to the AC
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
--- electra/__init__.py
+++ electra/__init__.py
@@ -13,14 +13,31 @@ logging.getLogger().setLevel(logging.INFO)
 
 logger = logging.getLogger(__name__)
 
+
 INTRUDER_LOCKOUT = "Intruder lockout"
 ATTR_DESC = "res_desc"
-ATTR_DATA = 'data'
-ATTR_SID = 'sid'
-ATTR_RES = 'res'
-ATTR_STATUS = 'status'
-ATTR_DEVICES = 'devices'
-RESPONSE_SUCCESS = 0
+ATTR_DATA = "data"
+ATTR_TOKEN = "token"
+ATTR_SID = "sid"
+ATTR_SID = "sid"
+ATTR_RES = "res"
+ATTR_STATUS = "status"
+ATTR_DEVICES = "devices"
+STATUS_SUCCESS = 0
+
+
+OPER_MODE_COOL = "COOL"
+OPER_MODE_HEAT = "HEAT"
+OPER_MODE_AUTO = "AUTO"
+OPER_MODE_DRY = "DRY"
+OPER_MODE_FAN = "FAN"
+
+OPER_FAN_SPEED_LOW = "LOW"
+OPER_FAN_SPEED_MED = "MED"
+OPER_FAN_SPEED_HIGH = "HIGH"
+OPER_FAN_SPEED_AUTO = "AUTO"
+OPER_ON = "ON"
+OPER_OFF = "OFF"
 
 """
 oper_state = {
@@ -86,105 +103,134 @@ resp = {
 }
 """
 
+MAX_TEMP = 30
+MIN_TEMP = 17
 
-DELAY_BETWEEM_SID_REQUESTS = int(timedelta(minutes=5).total_seconds() * 1000) # 5 minutes 
-SID_EXPIRATION = int(timedelta(hours=1).total_seconds() * 1000) # 1 hour 
+DELAY_BETWEEM_SID_REQUESTS = int(timedelta(minutes=5).total_seconds())  # 5 minutes
+SID_EXPIRATION = int(timedelta(hours=1).total_seconds())  # 1 hour
 
-OPER_MODE_COOL = "COOL"
-OPER_MODE_HEAT = "HEAT"
-OPER_MODE_AUTO = "AUTO"
-OPER_MODE_DRY = "DRY"
-OPER_MODE_FAN = "FAN"
 
-OPER_FAN_SPEED_LOW = "LOW"
-OPER_FAN_SPEED_MED = "MED"
-OPER_FAN_SPEED_HIGH = "HIGH"
-OPER_FAN_SPEED_AUTO = "AUTO"
-OPER_ON = "ON"
-OPER_OFF = "OFF"
+class ElectraApiError(Exception):
+    pass
 
-class ElectraAC(object):
 
+def generate_imei():
+    minimum = int(math.pow(10, 7))
+    maximum = int(math.pow(10, 8) - 1)
+    return f"2b950000{str(math.floor(random.randint(minimum, maximum)))}"
+
+
+class ElectraAirConditioner(object):
     def __init__(self, data) -> None:
-        self.id = data['id']
-        self.name = data['name']
-        self.regdate = data['regdate']
-        self.model = data['model']
-        self.mac = data['mac']
-        self.sn = data['sn']
-        self.manufactor = data['manufactor']
-        self.type = data['deviceTypeName']
-        self.status = data['status']
-        self.token = data['deviceToken']
+        self.id = data["id"]
+        self.name = data["name"]
+        self.regdate = data["regdate"]
+        self.model = data["model"]
+        self.mac = data["mac"]
+        self.serial_number = data["sn"]
+        self.manufactor = data["manufactor"]
+        self.type = data["deviceTypeName"]
+        self.status = data["status"]
+        self.token = data["deviceToken"]
 
+        self.current_mode = None
         self.collected_measure = None
         self._oper_data = None
 
-    def get_sensor_temp(self):
+    def get_sensor_temperature(self):
         return self.collected_measure
 
+    def get_mode(self):
+        return self._oper_data["AC_MODE"]
+
     def set_mode(self, mode: str):
-        if mode in [ OPER_MODE_AUTO, OPER_MODE_COOL, OPER_MODE_DRY, OPER_MODE_FAN, OPER_MODE_HEAT]:
+        if mode in [
+            OPER_MODE_AUTO,
+            OPER_MODE_COOL,
+            OPER_MODE_DRY,
+            OPER_MODE_FAN,
+            OPER_MODE_HEAT,
+        ]:
             if mode != self._oper_data["AC_MODE"]:
                 self._oper_data["AC_MODE"] = mode
 
-    def set_hswing(self, enable: bool):
-        enabled = self._oper_data["HSWING"] == OPER_ON
-        if enabled != enable:
-            self._oper_data["HSWING"] = OPER_ON if enabled else OPER_OFF
+    def set_horizontal_swing(self, enable: bool):
+        self._oper_data["HSWING"] = OPER_ON if enable else OPER_OFF
 
-    def set_vswing(self, enable: bool):
-        enabled = self._oper_data["VSWING"] == OPER_ON
-        if enabled != enable:
-            self._oper_data["VSWING"] = OPER_ON if enabled else OPER_OFF
+    def set_vertical_swing(self, enable: bool):
+        self._oper_data["VSWING"] = OPER_ON if enable else OPER_OFF
+
+    def is_vertical_swing(self):
+        return self._oper_data["VSWING"] == OPER_ON
+
+    def is_horizontal_swing(self):
+        return self._oper_data["HSWING"] == OPER_ON
 
     def is_on(self):
-        return self._oper_data['TURN_ON_OFF'] == OPER_ON
-    
+        return self._oper_data["TURN_ON_OFF"] == OPER_ON
+
     def turn_on(self):
         if not self.is_on():
-            self._oper_data['TURN_ON_OFF'] = OPER_ON
+            self._oper_data["TURN_ON_OFF"] = OPER_ON
 
     def turn_off(self):
         if self.is_on():
-            self._oper_data['TURN_ON_OFF'] = OPER_OFF
+            self._oper_data["TURN_ON_OFF"] = OPER_OFF
 
-    def get_temp(self):
-        return int(self._oper_data['SPT'])
+    def get_temperature(self):
+        return int(self._oper_data["SPT"])
 
-    def set_temp(self, val):
-        if self.get_temp() != val:
-            self._oper_data['SPT'] = str(val)
+    def set_temperature(self, val: int):
+        if self.get_temperature() != val:
+            self._oper_data["SPT"] = str(val)
 
     def get_fan_speed(self):
-        return self._oper_data['FANSPD']
-    
-    def set_fan_speed(self, speed):
-        if speed in [OPER_FAN_SPEED_AUTO, OPER_FAN_SPEED_HIGH, OPER_FAN_SPEED_MED, OPER_FAN_SPEED_LOW]:
-            if speed != self._oper_data['FANSPD']:
-                self._oper_data['FANSPD'] = speed
+        return self._oper_data["FANSPD"]
 
+    def set_fan_speed(self, speed):
+        if speed in [
+            OPER_FAN_SPEED_AUTO,
+            OPER_FAN_SPEED_HIGH,
+            OPER_FAN_SPEED_MED,
+            OPER_FAN_SPEED_LOW,
+        ]:
+            if speed != self._oper_data["FANSPD"]:
+                self._oper_data["FANSPD"] = speed
+
+    def set_turbo_mode(self, enable: bool):
+        self._oper_data["TURBO"] = OPER_ON if enable else OPER_OFF
+
+    def get_turbo_mode(self):
+        return self._oper_data["SHABAT"] == OPER_ON
+
+    def set_shabat_mode(self, enable: bool):
+        self._oper_data["SHABAT"] = OPER_ON if enable else OPER_OFF
+
+    def get_shabat_mode(self):
+        return self._oper_data["SHABAT"] == OPER_ON
 
     def update_operation_states(self, data):
-        self._oper_data = json.loads(data['OPER'])['OPER']
-        measurments = json.loads(data['DIAG_L2'])['DIAG_L2']
-        if 'I_RAT' in measurments:
-            self.collected_measure = int(measurments['I_RAT'])
-        if 'I_CALC_AT' in measurments:
-            self.collected_measure = int(measurments['I_CALC_AT'])
+        self._oper_data = json.loads(data["OPER"])["OPER"]
+        measurments = json.loads(data["DIAG_L2"])["DIAG_L2"]
+        if "I_RAT" in measurments:
+            self.collected_measure = int(measurments["I_RAT"])
+        if "I_CALC_AT" in measurments:
+            self.collected_measure = int(measurments["I_CALC_AT"])
+
+        self.current_mode = measurments["O_ODU_MODE"]
 
     def get_operation_state(self):
-        if 'AC_STSRC' in self._oper_data:
-            self._oper_data['AC_STSRC'] = 'WI-FI'
-    
-        json_state = json.dumps({'OPER': self._oper_data})
+        if "AC_STSRC" in self._oper_data:
+            self._oper_data["AC_STSRC"] = "WI-FI"
+
+        json_state = json.dumps({"OPER": self._oper_data})
         return json_state
 
 
 class ElectraAPI(object):
-    def __init__(self, imei, token, websession):
-        self._base_url = 'https://app.ecpiot.co.il/mobile/mobilecommand'
-        self._sid = '7c451662355641829636e2f8d79221b0'
+    def __init__(self, websession, imei=None, token=None):
+        self._base_url = "https://app.ecpiot.co.il/mobile/mobilecommand"
+        self._sid = None
         self._imei = imei
         self._token = token
         self._sid_expiration = 0
@@ -192,174 +238,156 @@ class ElectraAPI(object):
         self._session = websession
         self._phone_number = None
 
-    async def _send_request(self, payload) -> dict:            
+    async def _send_request(self, payload) -> dict:
         try:
-            resp = await self._session.post(url=self._base_url, json=payload, headers={'user-agent': 'Electra Client'})
+            resp = await self._session.post(
+                url=self._base_url,
+                json=payload,
+                headers={"user-agent": "Electra Client"},
+            )
             json_resp = await resp.json(content_type=None)
         except asyncio.TimeoutError as e:
-            logger.error('Failed to communicate with Electra API due to time out')
-            return None
+            raise ElectraApiError(
+                "Failed to communicate with Electra API due to time out"
+            )
         except aiohttp.ClientError as e:
-            logger.error('Failed to communicate with Electra API due to client error')
-            return None
+            raise ElectraApiError(
+                "Failed to communicate with Electra API due to client error"
+            )
         except JSONDecodeError as e:
-            logger.error('Recieved invalid response from Electra API: %s', e)
-            return None
-        else:
-            return json_resp
+            raise ElectraApiError("Recieved invalid response from Electra API: %s", e)
 
-    async def generate_token_step1(self, phone_number):
-        self._phone_number = phone_number 
-        def generate_imei():
-            minimum = int(math.pow(10, 7))
-            maximum = int(math.pow(10, 8) - 1)
-            return f'2b950000{str(math.floor(random.randint(minimum, maximum)))}'
-    
+        return json_resp
+
+    async def generate_new_token(self, phone_number, imei):
+
         payload = {
-					'pvdid': 1,
-					'id': 99,
-					'cmd': 'SEND_OTP',
-					'data': {
-						'imei': generate_imei(),
-						'phone': phone_number
-					}
-				}
+            "pvdid": 1,
+            "id": 99,
+            "cmd": "SEND_OTP",
+            "data": {"imei": imei, "phone": phone_number},
+        }
 
-        
         return await self._send_request(payload=payload)
 
-    async def generate_token_step2(self, otp):
+    async def validate_one_time_password(self, otp, imei, phone_number):
         payload = {
-					'pvdid': 1,
-					'id': 99,
-					'cmd': 'CHECK_OTP',
-					'data': {
-						'imei': self._imei,
-						'phone': self._phone_number,
-						'code': otp,
-						'os': 'android',
-						'osver': 'M4B30Z'
-					}
-				}
-            
+            "pvdid": 1,
+            "id": 99,
+            "cmd": "CHECK_OTP",
+            "data": {
+                "imei": imei,
+                "phone": phone_number,
+                "code": otp,
+                "os": "android",
+                "osver": "M4B30Z",
+            },
+        }
+
         return await self._send_request(payload=payload)
 
     def _sid_expired(self) -> bool:
-        if self._sid and int(datetime.now().timestamp() * 1000) < self._sid_expiration:
+        current_time = int(datetime.now().timestamp())
+        refresh_in = self._sid_expiration - current_time
+        if refresh_in > 0:
+            logger.debug("Should refresh in %s minutes", round(refresh_in / 60))
+
+        if current_time < self._sid_expiration:
             return False
         else:
             self._sid = None
             return True
-        
 
     async def _get_sid(self, force=False) -> bool:
 
-        current_ts = int(datetime.now().timestamp() * 1000)
+        current_ts = int(datetime.now().timestamp())
         if not force and not self._sid_expired():
-            logger.info('Found valid sid (%s) in cache, using it', self._sid)
-            return True
-        
-        if self._last_sid_request_ts and current_ts < (self._last_sid_request_ts + DELAY_BETWEEM_SID_REQUESTS):
-            print('Session ID was requested less than 5 minutes ago! waiting in order to prevent "intruder lockdown"...')
-            return False
+            logger.debug("Found valid sid (%s) in cache, using it", self._sid)
+            return
+
+        if self._last_sid_request_ts and current_ts < (
+            self._last_sid_request_ts + DELAY_BETWEEM_SID_REQUESTS
+        ):
+            logger.debug(
+                'Session ID was requested less than 5 minutes ago! waiting in order to prevent "intruder lockdown"...'
+            )
 
         payload = {
-			'pvdid': 1,
-			'id': 99,
-			'cmd': 'VALIDATE_TOKEN',
-			'data': {
-				'imei': self._imei,
-				'token': self._token,
-				'os': 'android',
-				'osver': 'M4B30Z'
-			}
-		}
+            "pvdid": 1,
+            "id": 99,
+            "cmd": "VALIDATE_TOKEN",
+            "data": {
+                "imei": self._imei,
+                "token": self._token,
+                "os": "android",
+                "osver": "M4B30Z",
+            },
+        }
 
         resp = await self._send_request(payload=payload)
         if resp is None:
-            logger.error('Failed to retrieve sid')
-            return False
+            raise ElectraApiError("Failed to retrieve sid")
         else:
-            if resp[ATTR_STATUS] != RESPONSE_SUCCESS:
-                logger.error('Failed to retrieve SID due to %s', resp[ATTR_DESC])
-                return False
+            if not resp[ATTR_DATA][ATTR_SID]:
+                raise ElectraApiError(
+                    "Failed to retrieve SID due to %s", resp[ATTR_DATA][ATTR_DESC]
+                )
+
             else:
-                self._sid = resp[ATTR_DATA][ATTR_SID] 
+                self._sid = resp[ATTR_DATA][ATTR_SID]
                 self._sid_expiration = current_ts + SID_EXPIRATION
                 self._last_sid_request_ts = current_ts
-                logger.info('Successfully acquired sid: %s', self._sid)
-                return True
-
+                logger.debug("Successfully acquired sid: %s", self._sid)
+                return
 
     async def get_devices(self):
         if self._sid_expired():
-            if (not await self._get_sid()):
-                logger.error("Failed to new session id, can't proceed with getting devices")
-                return None
-            
-        payload = {
-			'pvdid': 1,
-			'id': 99,
-			'cmd': 'GET_DEVICES',
-			'sid': self._sid
-		}
+            await self._get_sid()
+
+        payload = {"pvdid": 1, "id": 99, "cmd": "GET_DEVICES", "sid": self._sid}
 
         ac_list = []
         resp = await self._send_request(payload=payload)
-        if resp[ATTR_STATUS] == RESPONSE_SUCCESS:
+        if resp[ATTR_STATUS] == STATUS_SUCCESS:
             for ac in resp[ATTR_DATA][ATTR_DEVICES]:
-                if ac['deviceTypeName'] == 'A/C':
-                    ac_list.append(ElectraAC(ac, self))
-            
+                if ac["deviceTypeName"] == "A/C":
+                    ac_list.append(ElectraAirConditioner(ac))
+
             return ac_list
 
         else:
-            logger.error('Failed to fetch devices %s', resp)
-            return None
+            raise ElectraApiError("Failed to fetch devices %s", resp)
 
-    async def get_last_telemtry(self, ac: ElectraAC):
-    
+    async def get_last_telemtry(self, ac: ElectraAirConditioner):
+
         if self._sid_expired():
-            if (not await self._get_sid()):
-                logger.error("Failed to get session id, can't proceed with getting state")
-                return None
+            await self._get_sid()
 
         payload = {
-			'pvdid': 1,
-			'id': 99,
-			'cmd': 'GET_LAST_TELEMETRY',
-			'sid': self._sid,
-            'data': {
-                'id': ac.id,
-                'commandName': 'OPER,DIAG_L2'
-            }
-		}
+            "pvdid": 1,
+            "id": 99,
+            "cmd": "GET_LAST_TELEMETRY",
+            "sid": self._sid,
+            "data": {"id": ac.id, "commandName": "OPER,DIAG_L2"},
+        }
 
         resp = await self._send_request(payload=payload)
-        if resp[ATTR_STATUS] != RESPONSE_SUCCESS:
-            logger.error('Failed to get AC operation state')
+        if resp[ATTR_STATUS] != STATUS_SUCCESS:
+            raise ElectraApiError(f"Failed to get AC operation state: {resp}")
         else:
-            ac.update_operation_states(resp[ATTR_DATA]['commandJson'])
-    
+            ac.update_operation_states(resp[ATTR_DATA]["commandJson"])
 
-    async def set_state(self, device: ElectraAC):
+    async def set_state(self, device: ElectraAirConditioner):
         json_command = device.get_operation_state()
         if self._sid_expired():
-            if (not await self._get_sid()):
-                logger.error("Failed to get session id, can't proceed with setting state")
-                return None
+            await self._get_sid()
 
         payload = {
-			'pvdid': 1,
-			'id': 99,
-			'cmd': 'SEND_COMMAND',
-			'sid': self._sid,
-            'data': {
-                'id': device.id,
-                'commandJson': json_command
-            }
-		}
+            "pvdid": 1,
+            "id": 99,
+            "cmd": "SEND_COMMAND",
+            "sid": self._sid,
+            "data": {"id": device.id, "commandJson": json_command},
+        }
 
-        print(payload)
-        result = await self._send_request(payload=payload)
-        print(result)
+        return await self._send_request(payload=payload)
