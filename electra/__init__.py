34ae6d4ebd22913077d15fac32a44155a1451ac1
Jafar Atili <at.jafar@outlook.com>
2022-06-13 11:26:35 +0300

    enhanced logs

--- electra/__init__.py
+++ electra/__init__.py
@@ -8,9 +8,6 @@ from json import JSONDecodeError
 import json
 import random
 
-logging.basicConfig()
-logging.getLogger().setLevel(logging.INFO)
-
 logger = logging.getLogger(__name__)
 
 
@@ -38,70 +35,6 @@ OPER_FAN_SPEED_HIGH = "HIGH"
 OPER_FAN_SPEED_AUTO = "AUTO"
 OPER_ON = "ON"
 OPER_OFF = "OFF"
-
-"""
-oper_state = {
-	'id': 99,
-	'status': 0,
-	'desc': None,
-	'data': {
-		'timeDelta': 15,
-		'commandJson': {
-			'OPER': '{"OPER":{"TURN_ON_OFF":"OFF","AC_MODE":"COOL","SPT":"24","FANSPD":"AUTO","VSWING":"OFF","SLEEP":"OFF","HSWING":"OFF","CLEAR_FILT":"OFF","IDU_PN":"","IFEEL":"OFF","MSGTYPE":"OPER","OP_VAL_ERR":"OK","SHABAT":"OFF","TIMER":"OFF","TURBO":"OFF"}}',
-			'DIAG_L2': '{"DIAG_L2":{"I_RAT":"24","O_ODU_MODE":"COOL","IDU_FAN":"AUTO","IDU_PN":"","MSGTYPE":"DIAG_L2","O_OAT":""}}'
-		},
-		'res': 0,
-		'res_desc': None
-	}
-}
-"""
-
-"""
-resp = {
-	'id': 99,
-	'status': 0,
-	'desc': None,
-	'data': {
-		'devices': [
-            {
-			'providerName': None,
-			'deviceTypeName': 'A/C',
-			'manufactor': 'Midea',
-			'photoId': None,
-			'permissions': 15,
-			'deviceTypeId': 1,
-			'name': 'סלון',
-			'status': 1,
-			'providerid': 1,
-			'latitude': None,
-			'longitude': None,
-			'location': None,
-			'sn': 'XXXXXX',
-			'mac': 'XXXXX',
-			'model': 'XXXXX',
-			'hwVersion': None,
-			'fmVersion': None,
-			'userId': 99999,
-			'manufactorId': 2,
-			'iconId': '1',
-			'hasImage': False,
-			'deviceToken': 'XXXXX',
-			'mqttId': 'XXXXX',
-			'enableEvents': True,
-			'isActivated': False,
-			'logLevel': None,
-			'lastIntervalActivity': None,
-			'PowerOnID': None,
-			'IsDebugMode': False,
-			'regdate': '2021-03-31T21:12:39',
-			'id': 9999
-		    }
-        ],
-		'res': 0,
-		'res_desc': None
-	}
-}
-"""
 
 MAX_TEMP = 30
 MIN_TEMP = 17
@@ -246,16 +179,18 @@ class ElectraAPI(object):
                 headers={"user-agent": "Electra Client"},
             )
             json_resp = await resp.json(content_type=None)
-        except asyncio.TimeoutError as e:
+        except asyncio.TimeoutError:
             raise ElectraApiError(
                 "Failed to communicate with Electra API due to time out"
             )
-        except aiohttp.ClientError as e:
+        except aiohttp.ClientError as ex:
             raise ElectraApiError(
-                "Failed to communicate with Electra API due to client error"
+                "Failed to communicate with Electra API due to client error: %s", str(ex)
             )
-        except JSONDecodeError as e:
-            raise ElectraApiError("Recieved invalid response from Electra API: %s", e)
+        except JSONDecodeError as ex:
+            raise ElectraApiError(
+                "Recieved invalid response from Electra API: %s", str(ex)
+            )
 
         return json_resp
 
